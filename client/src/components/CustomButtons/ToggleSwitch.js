27a7d4bd6c99bfab842e8cd845598e29f9045b79
John Levander <john.d.levander@gmail.com>
2023-12-05 23:26:49 -0500

    added shrink feature for features

--- client/src/components/CustomButtons/ToggleSwitch.js
+++ client/src/components/CustomButtons/ToggleSwitch.js
@@ -50,9 +50,9 @@ class ToggleSwitch extends Component {
 
         // Use default as a fallback theme if valid theme is not passed
         const switchTheme = (theme && isString(theme)) ? theme : 'default';
-
+        const switchEnabled = enabled ? "switch-enabled" : "switch-disabled"
         const switchClasses = classnames(
-            `switch switch-box switch--${switchTheme}`,
+            `switch switch-box switch--${switchTheme} ${switchEnabled}`,
             className
         )
 
@@ -60,6 +60,7 @@ class ToggleSwitch extends Component {
             'switch-toggle',
             `switch-toggle--${enabled ? 'on' : 'off'}`
         )
+
         let label, switchControl;
         if (wantsDivs) {
             label = <div className={"switch_label"}> {this.props.label}</div>;
