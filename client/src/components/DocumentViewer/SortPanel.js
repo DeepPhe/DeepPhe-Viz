b0b9b07b51ccedf473d15fddac77f88078644acc
Johnsd11 <34665038+Johnsd11@users.noreply.github.com>
2024-05-30 13:45:16 -0400

    - working on sort dropDown

--- client/src/components/DocumentViewer/SortPanel.js
+++ client/src/components/DocumentViewer/SortPanel.js
@@ -19,62 +19,6 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 //     },
 // }));
 
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#1976d2',
-        },
-        secondary: {
-            main: '#dc004e',
-        },
-        text: {
-            primary: '#000000',
-            secondary: '#FFFFFF',
-        },
-    },
-});
-
-const StyledMenu = styled((props) => (
-    <Menu
-        elevation={0}
-        anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
-        }}
-        transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-        }}
-        {...props}
-    />
-))(({ theme }) => ({
-    '& .MuiPaper-root': {
-        borderRadius: 6,
-        marginTop: theme.spacing(1),
-        minWidth: 180,
-        color:
-            theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-        boxShadow:
-            'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-        '& .MuiMenu-list': {
-            padding: '4px 0',
-        },
-        '& .MuiMenuItem-root': {
-            '& .MuiSvgIcon-root': {
-                fontSize: 18,
-                color: theme.palette.text.secondary,
-                marginRight: theme.spacing(1.5),
-            },
-            '&:active': {
-                backgroundColor: alpha(
-                    theme.palette.primary.main,
-                    theme.palette.action.selectedOpacity,
-                ),
-            },
-        },
-    },
-}));
-
 
 export function SortPanel(props) {
     const { filteredConcepts, setFilteredConcepts } = props;
@@ -130,35 +74,38 @@ export function SortPanel(props) {
                     aria-haspopup="true"
                     aria-expanded={open ? 'true' : undefined}
                     variant="contained"
-                    disableElevation
+                    // disableElevation
                     onClick={handleClick}
-                    // endIcon={<KeyboardArrowDownIcon />}
+                    endIcon={<KeyboardArrowDownIcon />}
+                    style={{margin: 'auto', marginTop: '5%', display: "flex"}}
                 >
-                    Options
+                    Sort By
                 </Button>
-            <ThemeProvider theme={theme}>
-                <StyledMenu
+
+                <Menu
                     id="demo-customized-menu"
-                    MenuListProps={{
-                        'aria-labelledby': 'demo-customized-button',
-                    }}
+                    // MenuListProps={{
+                    //     'aria-labelledby': 'demo-customized-button',
+                    // }}
                     anchorEl={anchorEl}
                     open={open}
                     onClose={handleClose}
+
                 >
-                    <MenuItem onClick={handleClose} disableRipple>
+                    <MenuItem onClick={handleClose}>
                         Alphabetically
                     </MenuItem>
-                    <MenuItem onClick={handleClose} disableRipple>
+                    <MenuItem onClick={handleClose}>
                         Occurrence
                     </MenuItem>
-                    <MenuItem onClick={handleClose} disableRipple>
+                    <MenuItem onClick={handleClose}>
                         Semantic Group
                     </MenuItem>
-                    <MenuItem onClick={handleClose} disableRipple>
+                    <MenuItem onClick={handleClose}>
                         Confidence
                     </MenuItem>
-                </StyledMenu>
+                    </Menu>
+
         {/*<FormControl>*/}
         {/*    <FormLabel sx={{ fontWeight: 'light', fontSize: '1em', marginBottom: '-5px'}}><b>Sort Order:</b></FormLabel>*/}
         {/*    <CustomRadioGroup*/}
@@ -174,7 +121,6 @@ export function SortPanel(props) {
         {/*        <FormControlLabel value="confidence" control={<Radio size='small'/>} label="Confidence" />*/}
         {/*    </CustomRadioGroup>*/}
         {/*</FormControl>*/}
-                </ThemeProvider>
         </GridItem>
 
     );
