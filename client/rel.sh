cbecf9ea3edab0839d7bc955d21a0be8adca6232
John Levander <john.d.levander@gmail.com>
2025-08-27 14:02:12 -0400

    updated personal release script

--- client/rel.sh
+++ client/rel.sh
@@ -6,4 +6,6 @@ nvm use 16.16
 export NODE_OPTIONS=--max_old_space_size=4096
 npm run build
 pkg .
+cp client-macos ~/dev/dphe-installer/viz-output/DeepPhe-Viz/DeepPheVizClient2
+cp client-win.exe ~/dev/dphe-installer/viz-output/DeepPhe-Viz/DeepPheVizClient2.exe
 
